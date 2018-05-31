2eda436802235339387be6bdc7a56ced07bde848
Shuhao Wu <shuhao@shuhaowu.com>
2018-05-31 18:50:30 -0400

    Deal with ssh prompt

--- setup-all.sh
+++ setup-all.sh
@@ -31,4 +31,8 @@ sudo apt-get update
 sudo apt-get install -y python python3-pip libssl-dev
 sudo pip3 install -U ansible cryptography
 
+# Assume we are working in a secure network such that this is not a problem.
+# Otherwise we'll get spammed by a bunch of ssh key verifications from all
+# the hosts.
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i inventory -e 'ansible_python_interpreter=/usr/bin/python3' provision.yml
