37c32dde621839de60b64805b5eb40f36f587aa7
Shuhao Wu <shuhao@shuhaowu.com>
2018-05-31 11:25:05 -0400

    Setting up cluster with ansible

---
+++ setup-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ ! -f inventory ]; then
+  echo "ERROR: missing inventory file in current directory." >&2
+  exit 1
+fi
+
+if [ -z "$SSH_AUTH_SOCK" ]; then
+  echo "ERROR: you must run this with a SSH connection with agent forwarding" >&2
+  exit 1
+fi
+
+set -e
+
+################################
+# Generating SSH key for login #
+################################
+
+if [ ! -f roles/openfoam-base/files/id_rsa ]; then
+  mkdir -p roles/openfoam-base/files
+  ssh-keygen -f roles/openfoam-base/files/id_rsa -N ""
+fi
+
+#######################################
+# Setting up present node for ansible #
+#######################################
+
+echo "sudo is required to install ansible"
+
+sudo apt-get update
+sudo apt-get install -y python python3-pip libssl-dev
+sudo pip3 install -U ansible cryptography
+
+ansible-playbook -i inventory -e 'ansible_python_interpreter=/usr/bin/python3' provision.yml
